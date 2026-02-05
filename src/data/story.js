f961febdd8e59a29d6c11bd5989bdc930cd0ac66
Ahmed-Kharal <rafaykharal03@gmail.com>
2026-02-05 19:49:12 +0500

    Update story.js

--- src/data/story.js
+++ src/data/story.js
@@ -7,19 +7,19 @@ import img5 from "../assets/images/Image5.jpg";
 export const story = [
   {
     image: img1,
-    text: "Hiiii Goluuuu, Just wanted to tell you somethingggg...",
+    text: "Hiiii Goluuuu 🥰 Bas aik choti si baat kehni thi… Main dil se sorry kehna chahta hoon har us baat ke liye jis se ap mujh se gussa hui 🤍",
   },
   {
     image: img2,
-    text: "Do you remember this picture? Our first picture that you took ✨",
+    text: "Do you remember this picture? ✨ Our very first picture after we got into a relationship the one I took. Such a memorable day spent with you, full of love, happiness, and that sakoon you give me 🤍 I really, really love you, jaan.",
   },
   {
     image: img3,
-    text: "Our first trip as a hackathon... 🥺",
+    text: "Is pic ma to meri jan hai kyu ka ya meri favrt pic hai or is ma ma ny khud ko apni jan sath gaya hahaha or kitny mazay ki look dy rae hai isi ko daikh kr haas lo jan g... 🥺",
   },
   {
     image: img4,
-    text: "A day full of enjoyymenttt 😌",
+    text: "Hahahah 😂✨ Yeh image AI-generated hai, lekin dekho na… hum sath mein kitne ache lagte hain 😌❤️ Aur jaan, jo do log sath mein itne ache lagte hon, door door reh kar thori na ache lagte hain na 🥺 Toh pleaseee… aa jao na kareeb, meri pyari Reeb 🤍🫶",
   },
   {
     image: img5,
