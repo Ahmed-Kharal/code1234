8d6f713c15b7f4b066a830c343263f5a0170d350
Ahmed-Kharal <rafaykharal03@gmail.com>
2026-02-05 19:41:10 +0500

    Update story.js

--- src/data/story.js
+++ src/data/story.js
@@ -1,8 +1,8 @@
-import img1 from "../assets/images/img1.jpg";
-import img2 from "../assets/images/img2.jpg";
-import img3 from "../assets/images/img3.jpg";
-import img4 from "../assets/images/img4.jpg";
-import img5 from "../assets/images/img5.jpg";
+import img1 from "../assets/images/Image1.jpg";
+import img2 from "../assets/images/Image2.jpg";
+import img3 from "../assets/images/Image3.jpg";
+import img4 from "../assets/images/Image4.jpg";
+import img5 from "../assets/images/Image5.jpg";
 
 export const story = [
   {
